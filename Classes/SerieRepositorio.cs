dbf06f395b613b3a904657b30f54e5574ab81499
João Batista Santos Junior <joao.santos@programmers.com.br>
2022-01-03 16:44:54 -0300

    Add CRUD series in memory

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+
+namespace series.Classes
+{
+    public class SerieRepositorio : IRepository<Series>
+    {
+
+        private List<Series> listaSeries = new List<Series>();
+
+        public void Atualiza(int id, Series entidade)
+        {
+            listaSeries[id] = entidade;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSeries[id].Excluir();
+        }
+
+        public void Insere(Series entidade)
+        {
+            listaSeries.Add(entidade);
+        }
+
+        public List<Series> Lista()
+        {
+            return listaSeries;
+        }
+
+        public int ProximoId()
+        {
+            return listaSeries.Count;
+        }
+
+        public Series RetornaPorId(int id)
+        {
+            return listaSeries[id];
+        }
+
+        
+    }
+}
